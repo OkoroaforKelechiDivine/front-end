8b169373fbf33a74f3ba0652fd412b0e887890ac
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-10-07 08:47:47 +0100

    deploying to GitHub pages

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -93,8 +93,6 @@ function Home (){
                         <div className={"number"}>2</div>
                         <h1>Real Estate Management</h1>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
-                            tellus.
                             Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed
                             risus.
                             Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent tacitinubia nostra, per inceptos
