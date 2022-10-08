ad7821eda0e313ff9d27c83fc6835eb7c94e2c3c
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-10-08 10:02:32 +0100

    deploying to github pages

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const defaultState = {
   emailError:null,
   passwordError:null
 }
+
 class CustomFormValidation extends React.Component{
   constructor(props){
     super(props);
@@ -19,7 +20,7 @@ class CustomFormValidation extends React.Component{
   }
   handleInputChange(event) {
     const target = event.target;
-    var value = target.value;
+    const value = target.value;
     const name = target.name;
     this.setState({
       [name]: value
@@ -53,41 +54,59 @@ class CustomFormValidation extends React.Component{
   }
   render(){
     return(
-        <div className="App">
-          <div class="container-fluid ps-md-0">
-            <div class="row g-0">
-              <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
-              <div class="col-md-8 col-lg-6">
-                <div class="login d-flex align-items-center py-5">
-                  <div class="container">
-                    <div class="row">
-                      <div class="col-md-9 col-lg-8 mx-auto">
-                        <h3 class="login-heading mb-4">Welcome back!</h3>
+        <div className={"App"}>
+          <div className={"container-fluid ps-md-0"}>
+            <div className={"row g-0"}>
+              <div className={"d-none d-md-flex col-md-4 col-lg-6 bg-image"}></div>
+              <div className={"col-md-8 col-lg-6"}>
+                <div className={"login d-flex align-items-center py-5"}>
+                  <div className={"container"}>
+                    <div className={"row"}>
+                      <div className={"col-md-9 col-lg-8 mx-auto"}>
+                        <h3 className={"login-heading mb-4"}>Welcome back!</h3>
 
 
                         <form>
-                          <div class="form-floating mb-3">
-                            <input type="email" className={"form-control " + (this.state.emailError ? 'invalid' : '')} id="floatingInput" name='email' placeholder="name@example.com" value={this.state.email} onChange={this.handleInputChange} />
-                            <label for="floatingInput">Email address</label>
-                            <span className="text-danger">{this.state.emailError}</span>
+                          <div className={"form-floating mb-3"}>
+                            <input
+                                type={"email"}
+                                className={"form-control " + (this.state.emailError ? 'invalid' : '')}
+                                id={"floatingInput"}
+                                name={'email'}
+                                placeholder={"name@example.com"}
+                                value={this.state.email}
+                                onChange={this.handleInputChange} />
+                            <label form={"floatingInput"}>Email address</label>
+                            <span className={"text-danger"}>{this.state.emailError}</span>
                           </div>
-                          <div class="form-floating mb-3">
-                            <input type="password" className={"form-control " + (this.state.passwordError ? 'invalid' : '')} id="floatingPassword" name="password" placeholder="Password" value={this.state.password} onChange={this.handleInputChange} />
-                            <label for="floatingPassword">Password</label>
-                            <span className="text-danger">{this.state.passwordError}</span>
+                          <div className={"form-floating mb-3"}>
+                            <input
+                                type={"password"}
+                                className={"form-control " + (this.state.passwordError ? 'invalid' : '')}
+                                id={"floatingPassword"}
+                                name={"password"}
+                                placeholder={"Password"}
+                                value={this.state.password}
+                                onChange={this.handleInputChange} />
+                            <label form={"floatingPassword"}>Password</label>
+                            <span className={"text-danger"}>{this.state.passwordError}</span>
                           </div>
 
-                          <div class="form-check mb-3">
-                            <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck" />
-                            <label class="form-check-label" for="rememberPasswordCheck">
+                          <div className={"form-check mb-3"}>
+                            <input
+                                className={"form-check-input"}
+                                type={"checkbox"} value={""} id={"rememberPasswordCheck"} />
+                            <label className={"form-check-label"} form={"rememberPasswordCheck"}>
                               Remember password
                             </label>
                           </div>
 
-                          <div class="d-grid">
-                            <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="button" onClick={()=>this.submit()}>Sign in</button>
-                            <div class="text-center">
-                              <a class="small" href="#">Forgot password?</a>
+                          <div className={"d-grid"}>
+                            <button
+                                className={"btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2"}
+                                type={"button"} onClick={()=>this.submit()}>Sign in</button>
+                            <div className={"text-center"}>
+                              <a className={"small"} href={"#"}>Forgot password?</a>
                             </div>
                           </div>
 
