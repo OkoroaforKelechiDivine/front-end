46505d8a75945ac0291f2a011bb6b838f547e9ef
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-10-08 08:29:24 +0100

    editing text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,9 +5,9 @@ const HeroSection = () => {
     return (
         <section className={'hero-section'} >
             <div className={"content"}>
-                <h1>More than just shorter links</h1>
-                <p>Build your brand's recognition and get detailed insights on your links are performing.</p>
-                <button>Get Started</button>
+                <h1>Making things small is what I do</h1>
+                <p>Small is the new big </p>
+                <button>Sign up to receive news</button>
             </div>
             <div className={"main-illustration"}>
                 <img src={mainIllustration} alt={""}/>
