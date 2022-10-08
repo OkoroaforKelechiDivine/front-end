5f1fd123de902526e2ecc68dfd1683df3735f777
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-10-08 07:11:41 +0100

    fixed

---
+++ src/component/Link.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Link = ({originalLink, shortLink}) => {
+
+    const copyLink = (e) => {
+        navigator.clipboard.writeText(shortLink);
+        e.target.value = 'Copied!';
+        e.target.style.backgroundColor = ' hsl(257, 27%, 26%)';
+    }
+
+    return (
+        <li className='short-link'>
+            <p className="link">{originalLink}</p>
+            <span className="copy-short-link">
+          <p className="link">{shortLink}</p>
+          <input type='button' onClick={copyLink} value="Copy"/>
+        </span>
+        </li>
+    )
+}
+
+export default Link;
